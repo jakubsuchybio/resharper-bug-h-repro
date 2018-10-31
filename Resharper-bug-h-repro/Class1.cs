c70c4e80a439c69dbb219c4f17931f9dfee0dae2
Kuba <suchyj@btlnet.com>
2018-10-31 08:53:56 +0100

    Initial commit

---
+++ Resharper-bug-h-repro/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resharper_bug_h_repro
+{
+    public class Class1
+    {
+        private string x = HeaderFileClass.COMPANY;
+    }
+}
---
+++ Resharper-bug-h-repro/HeaderFile.h
@@ -0,0 +1,5 @@
+﻿
+public static class HeaderFileClass
+{
+    public const string COMPANY = "<COMPANY_NAME>";
+}
